5f8a03ffc60874f4b033ad5f6d2f9e4fe2b77a79
LEE Jaeyoung <jaeyoung@monodiary.net>
2023-10-03 16:18:19 +0900

    Create schema_migrations.sql

---
+++ schema_migrations.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
